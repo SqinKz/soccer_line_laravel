71010d23c50c089a4407141dab7a2ba6e23d6b74
SqinKz <adityaasp2@gmail.com>
2024-12-13 23:30:42 +0700

    First Init

---
+++ database/migrations/2024_12_11_202609_database_soccerline.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_program');
+            $table->string('kategori');
+            $table->string('informasi');
+            $table->string('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswa');
+    }
+};
